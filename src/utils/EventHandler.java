92a88b19459dea0c4ae7ec296b0b3b183dadef2f
Joulies <Joulies@Joul>
2015-06-25 21:22:07 +0300

    q

--- src/utils/EventHandler.java
+++ src/utils/EventHandler.java
@@ -6,6 +6,7 @@ import javafx.scene.input.MouseEvent;
 public class EventHandler implements javafx.event.EventHandler<MouseEvent> {
 
 	private EventHandlerAble eventHandlerAble = null;
+	private MouseEvent mouseEvent = null;
 
 	public interface EventHandlerAble {
 		public void handleMouseButtonPrimary();
@@ -22,9 +23,19 @@ public class EventHandler implements javafx.event.EventHandler<MouseEvent> {
 			if (!event.getButton().equals(MouseButton.PRIMARY))
 				return;
 
+		this.mouseEvent = event;
+
 		Executor.runLater(() -> this.eventHandlerAble
 				.handleMouseButtonPrimary());
 
+	}
+
+	public double getX() {
+		return this.mouseEvent.getX();
+	}
+
+	public double getY() {
+		return this.mouseEvent.getY();
 	}
 
 }
