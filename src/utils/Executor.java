bb4575b16ad28d89d00c0a4d074fde4c79699657
Joulies <Joulies@Joul>
2015-08-12 21:57:10 +0300

    q

--- src/utils/Executor.java
+++ src/utils/Executor.java
@@ -7,23 +7,46 @@ public class Executor {
 
 	private static ExecutorService executorService = Executors
 			.newCachedThreadPool();
+	private static ThreadRunning threadRunning = ThreadRunning.FX;
 
 	private Executor() {
 
 	}
 
 	public static void sleep(final long millis) {
-
 		try {
 			Thread.sleep(millis);
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
-
 	}
 
 	public static void runLater(Runnable runnable) {
+
+		switch (threadRunning) {
+
+		case EXECUTOR_SERVICE:
+			runLaterExecutorService(runnable);
+			break;
+
+		case FX:
+			runLaterFX(runnable);
+			break;
+
+		}
+
+	}
+
+	private static void runLaterExecutorService(Runnable runnable) {
 		executorService.submit(runnable);
+	}
+
+	private static void runLaterFX(Runnable runnable) {
+		runnable.run();
+	}
+
+	private enum ThreadRunning {
+		EXECUTOR_SERVICE, FX
 	}
 
 }
