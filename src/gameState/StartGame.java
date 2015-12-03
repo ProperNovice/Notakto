fae42be9e692d1bf6adb2eea4bfc6445813c255e
Joulies <Joulies@Joul>
2015-12-03 16:18:26 +0200

    q

--- src/gameState/StartGame.java
+++ src/gameState/StartGame.java
@@ -1,5 +1,7 @@
 package gameState;
 
+import utils.ArrayList;
+import enums.Element;
 import enums.GameStateEnum;
 
 public class StartGame extends GameState {
@@ -11,7 +13,21 @@ public class StartGame extends GameState {
 	@Override
 	public void handleGameStateChange() {
 
+		ArrayList<Element> list = new ArrayList<>();
+
+		list.add(Element.C);
+		list.add(Element.D);
+		list.add(Element.D);
+
+		list.printList();
+		list = super.controller.elementController().trimElements(list);
+
+		System.out.println("final list");
+		list.printList();
+		System.out.println();
+
+		System.out.println(super.controller.elementController()
+				.isPerfectPosition(list));
 
 	}
-
 }
