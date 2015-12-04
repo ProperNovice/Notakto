e6243300dace0f749a958903920b7d6d5f2e9117
Joulies <Joulies@Joul>
2015-12-04 19:33:07 +0200

    q

--- src/gameState/StartGame.java
+++ src/gameState/StartGame.java
@@ -34,7 +34,8 @@ public class StartGame extends GameState {
 
 		GameStateEnum gameStateEnum = super.controller.playerController()
 				.getCurrentPlayerType();
-		super.controller.playerController().setCurrentPlayerTextTurnVisible(true);
+		super.controller.playerController().setCurrentPlayerTextTurnVisible(
+				true);
 		super.controller.flow().addGameStateFirst(gameStateEnum);
 
 		super.controller.flow().proceedToNextPhase();
@@ -59,52 +60,6 @@ public class StartGame extends GameState {
 			Logger.logNewLine("perfect");
 		else
 			Logger.logNewLine("not perfect");
-
-	}
-
-	private void testBoards() {
-
-		// int total = 0;
-		// int totalNonActive = 0;
-		//
-		// int a = -1, b = -1, c = -1, d = -1, e = -1, f = -1;
-		//
-		// Board board = new Board();
-		//
-		// for (a = 0; a < 9; a++)
-		// for (b = a + 1; b < 9; b++)
-		// for (c = b + 1; c < 9; c++)
-		// for (d = c + 1; d < 9; d++)
-		// for (e = d + 1; e < 9; e++)
-		// for (f = e + 1; f < 9; f++)
-		//
-		// {
-		//
-		// board.setAllEmpty();
-		// board.setNonEmpty(a, b, c, d, e, f);
-		//
-		// total++;
-		//
-		// if (!board.isActive())
-		// continue;
-		//
-		// totalNonActive++;
-		//
-		// ArrayList<Element> elementList = super.controller
-		// .boardPatternsController()
-		// .getElementList(board);
-		//
-		// if (elementList != null)
-		// continue;
-		//
-		// Logger.logNewLine("not found");
-		// ShutDown.execute();
-		//
-		// }
-		//
-		// System.out.println(totalNonActive + " active");
-		// System.out.println(total - totalNonActive + " nonActive");
-		// System.out.println(total + " total");
 
 	}
 
